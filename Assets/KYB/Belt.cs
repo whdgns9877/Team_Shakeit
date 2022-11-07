209e30d8c172392f2dbf44f1a6688ff7e7938123
KIMYONGBIN <dragon@os21.com>
2022-11-07 12:40:56 +0900

    Update Belt.cs

--- Assets/KYB/Belt.cs
+++ Assets/KYB/Belt.cs
@@ -7,6 +7,6 @@ public class Belt : MonoBehaviour
     public bool isRight;
     private void OnCollisionStay2D(Collision2D collision)
     {
-        collision.transform.Translate((isRight ? Vector2.right : Vector2.left) * Time.deltaTime * 3f);
+        collision.transform.Translate((isRight ? Vector2.right : Vector2.left) * Time.deltaTime * 1.5f);
     }
 }
