719c585f309574e90e50874e2c81a266be051f5f
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2022-11-07 09:46:41 +0900

    ChangeSpriteMode

--- Assets/KJH/Player.cs
+++ Assets/KJH/Player.cs
@@ -7,15 +7,24 @@ public class Player : MonoBehaviour
     public enum PlayerState { Idle, Move, Jump, Dash}
 
     Rigidbody2D rb;
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] private float maxSpeed;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     private void FixedUpdate()
     {
         float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
         rb.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+        rb.AddForce(Vector2.right * v, ForceMode2D.Impulse);
+
+        if (rb.velocity.x > maxSpeed)
+            rb.velocity = new Vector2(maxSpeed, rb.velocity.y);
+        else if(rb.velocity.x < maxSpeed * -1f)
+            rb.velocity = new Vector2(maxSpeed * -1f, rb.velocity.y);
     }
 }
